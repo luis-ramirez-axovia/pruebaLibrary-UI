6b7fbdfc9761cb4ecb7078827c6779afffec5ba8
Luis Ramirez <luis.axovia@gmail.com>
2023-04-26 17:21:06 -0600

    First Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  ContainerFlex,
+  MediaImg,
+  Text,
+} from "@luis.axovia/libreria_ui";
+import { getCharacter } from "./service";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const [character, setCharacter] = useState("");
+
+  const gettingCharacter = async (character) => {
+    const response = await getCharacter(character);
+    console.log(
+      "🚀 ~ file: App.js:13 ~ gettingCharacter ~ response:",
+      response
+    );
+    setCharacter(response);
+  };
+
+  const handleDecrement = () => {
+    setNumber((prevCount) => prevCount - 1);
+  };
+  const handleIncrement = () => {
+    setNumber((prevCount) => prevCount + 1);
+  };
+
+  useEffect(() => {
+    gettingCharacter(number);
+  }, [number]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ContainerFlex
+        flexDirection="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+        height="100vh"
+        gap="8px"
+      >
+        <MediaImg
+          src={character.image}
+          alt={character.name}
+          width="200px"
+          height="200px"
+        />
+        <Text component="h3" fontSize="18px" lineHeight="10px">
+          {character.name}
+        </Text>
+        <Text component="p" fontSize="18px" lineHeight="10px">
+          {character.id}
+        </Text>
+        <Text component="p" fontSize="18px" lineHeight="10px">
+          {character.species}
+        </Text>
+        <Text component="p" fontSize="18px" lineHeight="10px">
+          {character.status}
+        </Text>
+        <ContainerFlex
+          flexDirection="row"
+          justifyContent="center"
+          alignContent="center"
+          height="40px"
+          gap="8px"
         >
-          Learn React
-        </a>
-      </header>
+          <Button
+            onClick={handleDecrement}
+            width="150px"
+            height="40px"
+            bgColor="lighsteelblue"
+            bgColorHover="lightcoral"
+          >
+            {"< Before"}
+          </Button>
+          <Button
+            onClick={handleIncrement}
+            width="150px"
+            height="40px"
+            bgColor="lighsteelblue"
+            bgColorHover="lightgreen"
+          >
+            {"Next >"}
+          </Button>
+        </ContainerFlex>
+      </ContainerFlex>
     </div>
   );
 }
